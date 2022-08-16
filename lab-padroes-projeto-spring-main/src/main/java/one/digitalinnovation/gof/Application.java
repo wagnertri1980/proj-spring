f8fb1465682a41f4d3c66b29c089e1046dc9cbb1
Wagnertri19802016 <wagnertri1980@gmail.com>
2022-08-16 10:38:02 -0300

    Add files via upload

---
+++ lab-padroes-projeto-spring-main/src/main/java/one/digitalinnovation/gof/Application.java
@@ -0,0 +1,21 @@
+package one.digitalinnovation.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * Projeto Spring Boot gerado via Spring Initializr.
+ * 
+ *   
+ * @author Wagner Souza de Lima
+ */
+@EnableFeignClients
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
